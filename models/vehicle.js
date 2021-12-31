d03c23dbcc41fa5ced586074933ad452863d78e6
Edmond Chen <chen.edmond13@gmail.com>
2021-12-30 16:39:58 -0800

    hid mongodb uri

--- models/vehicle.js
+++ models/vehicle.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const url = "mongodb+srv://fleetcare-user:Edmo1508@cluster0.ltn6m.mongodb.net/userVehicles?retryWrites=true&w=majority"
+const url = process.env.MONGODB_URI 
 
 mongoose.connect(url)
     .then(result => {
