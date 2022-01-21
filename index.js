662d8a5359f91041be43a4eedc94bcf90dae79cd
Edmond Chen <84605666+echen12@users.noreply.github.com>
2022-01-21 12:29:23 -0800

    code cleanup

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const dotenv = require("dotenv")
-dotenv.config()
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
 const app = express();
-const auth = require("./routes/auth")
-const vehicleInfo = require("./routes/vehicle-info")
-const cors = require('cors')
+const auth = require("./routes/auth");
+const vehicleInfo = require("./routes/vehicle-info");
+const cors = require('cors');
 
 
-app.use(cors())
+app.use(cors());
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(express.static('build'))
+app.use(express.static('build'));
 
-app.use("/auth", auth)
+app.use("/auth", auth);
 
-app.use("/api", vehicleInfo)
+app.use("/api", vehicleInfo);
 
-const PORT = process.env.PORT
-app.listen(PORT)
-console.log(`Server is running on port ${PORT}`)
+const PORT = process.env.PORT;
+app.listen(PORT);
+console.log(`Server is running on port ${PORT}`);
