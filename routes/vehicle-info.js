b4219d6e3b20395810fb933a21412a16d34ad2f6
Edmond Chen <84605666+echen12@users.noreply.github.com>
2022-01-21 12:28:43 -0800

    code cleanup

--- routes/vehicle-info.js
+++ routes/vehicle-info.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const Vehicle = require("../models/vehicle");
-const checkAuth = require("../middleware/checkAuth")
+const checkAuth = require("../middleware/checkAuth");
 
 router.get("/vehicle-info", checkAuth, async (req, res) => {
     await Vehicle.findById(req.query.key).then(data => {
@@ -8,44 +8,29 @@ router.get("/vehicle-info", checkAuth, async (req, res) => {
     })
 }) 
 
-// // get individual vehicle details
-// router.get("/vehicle-info/:id", checkAuth, async (req, res) => {
-//     const veh = await Vehicle.findById(req.params.id);
-//     console.log(veh)
-// })
-
 router.post("/add-vehicle", checkAuth, async (req, res) => {
-    const id = req.body.id
-    const vehData = req.body.obj
-    //console.log(id)
-    //console.log("ID " + req.body.id)
-    //console.log(req.body)
-    // await Vehicle.findOneAndUpdate(id,{test: "tet"}).then(res => {
-    //     //console.log(res)
-    //     //res.json(req.body)
-    // })
-
+    const id = req.body.id;
+    const vehData = req.body.obj;
     const veh = await Vehicle.findById(id);
 
-    veh.vehicleInfo = vehData
+    veh.vehicleInfo = vehData;
 
     await veh.save();
-    res.json(veh)
-
+    res.json(veh);
 })
 
 router.put("/update-vehicle/:id", checkAuth, async (req, res) => {
     const veh = await Vehicle.findById(req.body.userId);
-    veh.vehicleInfo = req.body.obj
+    veh.vehicleInfo = req.body.obj;
     await veh.save();
-    res.json(veh)
+    res.json(veh);
 })
 
 router.put("/delete-vehicle", checkAuth, async (req, res) => {
     const veh = await Vehicle.findById(req.body.userId);
-    veh.vehicleInfo = req.body.obj
+    veh.vehicleInfo = req.body.obj;
     await veh.save();
-    res.json(veh)
+    res.json(veh);
 })
 
 module.exports = router;     
