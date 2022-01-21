d9a1bb881bda633a53d42ba6a5d1573250b02ac0
Edmond Chen <84605666+echen12@users.noreply.github.com>
2022-01-21 12:24:37 -0800

    code cleanup

--- models/vehicle.js
+++ models/vehicle.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
-const url = process.env.MONGODB_URI 
+const url = process.env.MONGODB_URI;
 
 mongoose.connect(url) 
     .then(result => {
-        console.log('connected to MongoDB')
+        console.log('connected to MongoDB');
     })
     .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
+        console.log('error connecting to MongoDB:', error.message);
     })
 
 const vehicleSchema = new mongoose.Schema({
@@ -24,10 +24,10 @@ const vehicleSchema = new mongoose.Schema({
 
 vehicleSchema.set('toJSON', {
     transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
     }
 })
 
-module.exports = mongoose.model('Vehicle', vehicleSchema)
+module.exports = mongoose.model('Vehicle', vehicleSchema);
